d029b9e993f1049e6a7c07e034a21f60c882b9b8
Eckhardt Dreyer <support@kaizenmedia.co.za>
2021-12-15 18:14:49 +0200

    feat: seo on posts

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -41,6 +41,37 @@ export default {
       next,
     }
   },
+  head() {
+    return {
+      title: this.post.title,
+      description: this.post.description,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.post.description,
+        },
+        // Open Graph
+        { hid: 'og:title', property: 'og:title', content: this.post.title },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: this.post.description,
+        },
+        // Twitter Card
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: this.post.title,
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: this.post.description,
+        },
+      ],
+    }
+  },
 }
 </script>
 
